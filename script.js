56d17427463e904aee502482ca4e3f1f0cec66d4
cwklausing <cwklausing@gmail.com>
2015-11-18 16:43:50 -0600

    Randomization is successful

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function Peer (fullName) {
 /*creates first and last names */
 	this.fullName = fullName;
 
-
 /*Every time a new peer is created, totalPeers goes up by one*/
 	totalPeers++;
 };
@@ -18,6 +17,30 @@ function pushToArray (fullName) {
 	peerArray.push(new Peer(fullName));
 }
 
+/*team shuffling function*/
+function shuffle(array) {
+	var tempArray = array.slice();
+	var shuffledTeam = tempArray.length;
+	var temporaryValue;
+	var randomIndex;
+
+	 // While there remain elements to shuffle...
+	 while (0 !== shuffledTeam) {
+
+	   // Pick a remaining element...
+	   randomIndex = Math.floor(Math.random() * shuffledTeam);
+	   shuffledTeam -= 1;
+
+	   // And swap it with the current element.
+	   temporaryValue = tempArray[shuffledTeam];
+	   tempArray[shuffledTeam] = tempArray[randomIndex];
+	   tempArray[randomIndex] = temporaryValue;
+	}
+	return tempArray;
+}
+
+
+console.log(peerArray);
 /*pushes all classmates to peerArray array*/
 pushToArray('Amanda Bausch');
 pushToArray('Ashley Steele');
@@ -42,26 +65,9 @@ pushToArray('James Kirwin');
 pushToArray('Manuel Zumarraga');
 pushToArray('Sam Richard');
 
+var shuffledClassList = shuffle(peerArray);
 
-/*team shuffling function*/
-var newTeam = function shuffle(array) {
 
-	var shuffledTeam = peerArray.length, temporaryValue, randomIndex ;
+console.log(shuffledClassList.slice(0,7));
+console.log(shuffledClassList.slice(0,7));
 
-	 // While there remain elements to shuffle...
-	 while (0 !== shuffledTeam) {
-
-	   // Pick a remaining element...
-	   randomIndex = Math.floor(Math.random() * shuffledTeam);
-	   shuffledTeam -= 1;
-
-	   // And swap it with the current element.
-	   temporaryValue = peerArray[shuffledTeam];
-	   peerArray[shuffledTeam] = peerArray[randomIndex];
-	   peerArray[randomIndex] = temporaryValue;
-	}
-
-	 return peerArray;
-}
-
-console.log(peerArray);
